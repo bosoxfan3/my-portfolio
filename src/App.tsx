d3a4425a86169a30409247bc7791b8f59a621cf0
Daniel Acquesta <dgacquesta@gmail.com>
2025-04-03 16:09:29 -0700

    got prettier formatting finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import Projects from "./pages/projects";
-import Contact from "./pages/contact";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 };
 
 export default App;
