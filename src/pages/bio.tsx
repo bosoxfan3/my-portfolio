a7c7c61ca87b735516b8e6c2d6fb25f6d9e0bf2d
Daniel Acquesta <dgacquesta@gmail.com>
2025-04-23 10:49:38 -0700

    space out bio text and add another email link

--- src/pages/bio.tsx
+++ src/pages/bio.tsx
@@ -24,16 +24,35 @@ const Bio = () => (
                     I'm a Senior Front End Engineer with 7+ years of
                     professional experience.
                 </h2>
-                <p className="text-center pb-6">
-                    I specialize in working on user-facing single-page
-                    applications with React & TypeScript. Throughout my journey
-                    I've also gained proficiency with JavaScript, HTML, CSS,
-                    Node.js, Next.js, etc. Over the last 2 years I have worked
-                    as a full stack engineer, with my backend experience
-                    focusing on SQL, PostgreSQL, and RESTful APIs. I'm looking
-                    for full time front end or full stack roles in the San
-                    Francisco Bay Area.
-                </p>
+                <div className="text-center pb-6">
+                    <p>
+                        I specialize in working on user-facing single-page
+                        applications with React & TypeScript.
+                    </p>
+                    <br />
+                    <p>
+                        Throughout my journey I've also gained proficiency with
+                        JavaScript, HTML, CSS, Node.js, Next.js, etc. Over the
+                        last 2 years I have worked as a full stack engineer,
+                        with my backend experience focusing on SQL, PostgreSQL,
+                        and RESTful APIs.
+                    </p>
+                    <br />
+                    <p>
+                        I'm looking for full time front end or full stack roles
+                        in the San Francisco Bay Area. Thanks for stopping by.
+                        Let's build something people love to use!
+                    </p>
+                </div>
+                <div className="text-center text-lg pb-6 font-bold underline hover:text-blue-500 hover:cursor-pointer transition-colors duration-300">
+                    <a
+                        href="mailto:dgacquesta@gmail.com"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        Contact Me
+                    </a>
+                </div>
                 <div className="text-center text-lg pb-6 font-bold underline hover:text-blue-500 transition-colors duration-300">
                     <Link to="/projects">See My Work</Link>
                 </div>
