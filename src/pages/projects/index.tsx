5ee8be550cced9bbc4822a6808d97c1f3a1ce4d6
Daniel Acquesta <dgacquesta@gmail.com>
2025-05-25 11:34:59 -0700

    updated project card click routing

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -22,7 +22,7 @@ const Projects = () => (
                     title="Prompt Music"
                     image="/assets/personal/prompt-music/main.png"
                     shortDescription="This app allows users to generate a playlist based on a text input. If they like the playlist, they can authorize the app to access their Spotify account and save the playlist to their own Spotify."
-                    route="/projects/prompt-music"
+                    route="/projects/personal/prompt-music"
                 />
             </div>
         </div>
@@ -51,37 +51,37 @@ const Projects = () => (
                     title="Betslip & Quickslip Implementation"
                     image="/assets/action/betslip/main.png"
                     shortDescription="The core functionality of the Action Network products is the ability to save picks via the betslip. Quickslip is an add-on integration that syncs a user's bets with their sportsbook account."
-                    route="/projects/betslip"
+                    route="/projects/action/betslip"
                 />
                 <ProjectCard
                     title="My Action Page"
                     image="/assets/action/my-action/main.png"
                     shortDescription="The My Action page is where a user can see all the picks they've tracked and can run analysis on their betting history."
-                    route="/projects/my-action"
+                    route="/projects/action/my-action"
                 />
                 <ProjectCard
                     title="Odds Tools"
                     image="/assets/action/odds-tools/main.png"
                     shortDescription="Worked on rendering and filtering real-time odds data for multiple sports and books. The odds tools are the most frequented pages on the Action Network site."
-                    route="/projects/odds-tools"
+                    route="/projects/action/odds-tools"
                 />
                 <ProjectCard
                     title="Odds Calculators"
                     image="/assets/action/odds-calculators/main.png"
                     shortDescription="Interactive betting calculators that handle real-time odds calculations and edge case validations. Currently the #1 Google search response for 'odds calculator'."
-                    route="/projects/odds-calculators"
+                    route="/projects/action/odds-calculators"
                 />
                 <ProjectCard
                     title="Expert Picks Page"
                     image="/assets/action/expert-picks/main.png"
                     shortDescription="Built this entire page from scratch using React and Tailwind. This page allows users to browse expert picks and popular matchups."
-                    route="/projects/expert-picks"
+                    route="/projects/action/expert-picks"
                 />
                 <ProjectCard
                     title="Article Page Redesign"
                     image="/assets/action/articles/main.png"
                     shortDescription="Modernized our longform content experience with performance in mind and reusable component systems. Created components that authors could quickly embed to enhace their content."
-                    route="/projects/articles"
+                    route="/projects/action/articles"
                 />
             </div>
         </div>
