a155c9377abab6d7cc4cbf5efed13c7660fdba2d
Daniel Acquesta <dgacquesta@gmail.com>
2025-06-01 15:49:40 -0700

    use browserrouter instead of hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Bio from './pages/bio';
 import Projects from './pages/projects';
 import PromptMusic from './pages/projects/personal/prompt-music';
